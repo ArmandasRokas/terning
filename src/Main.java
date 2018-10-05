6fe830eda43fc0f14352f4449b723831ccb4e508
Armandas <armandas.rokas@gmail.com>
2018-10-05 10:29:01 +0200

    print

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	// test
-
-
-        // bug is fixed
+        // write your code here
+        //test1
+        //testKNA1
+        //Kristian er flot
     }
 
 
